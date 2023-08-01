b3dd6614e8b966e212e250f2f45627a72aed722b
zura magalashvili <zuramagalashvili0@gmail.com>
2023-08-01 18:42:05 +0400

    day6 objects

---
+++ Day-6/hw2.js
@@ -0,0 +1,41 @@
+var Books = [
+    {
+      author: 'Bill',
+      title: 'The Road Ahead',
+      haveRead: true,
+      dateOfRead: new Date(2020, 10, 10)
+    },
+    {
+      author: 'Steve',
+      title: 'Walter Isaacson',
+      haveRead: true,
+      dateOfRead: new Date(2020, 10, 5)
+    },
+    {
+      author: 'Jhon',
+      title:  'The Hunger Games',
+      haveRead: false,
+      dateOfRead: NaN
+    }
+  ];
+  
+  function readingStatus(books) {
+    for (const book of books) {
+      if (book.haveRead) {
+          Object.defineProperty(book, "daysAgo",{
+            get(){
+            const today = new Date();
+            const diffTime = today - book.dateOfRead;
+            const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
+            return `${diffDays} days ago`;
+            }
+          })
+        console.log(`${book.author} have read ${book.title} book ${book.daysAgo}`);
+      } else {
+        console.log(`${book.author} haven't read ${book.title} book yet`);
+      }
+    }
+  }
+  
+  readingStatus(Books);
+  
